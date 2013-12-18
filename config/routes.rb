85fc628a386624ed8770d2a86acf19b5fc41a9bf
Jason Anderson <anderson.l.jason@gmail.com>
2013-12-18 13:50:20 -0800

    Added dashboard controller base

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Graf::Application.routes.draw do
 
   resources :repos
 
+  get '/dashboard', to: 'dashboard#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
