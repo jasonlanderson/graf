34769b8eab04f5e7a0c22eb031092f7f8a0af907
Jason Anderson <anderson.l.jason@gmail.com>
2014-01-08 00:00:17 -0800

    Refactored dashboard

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,56 +1,57 @@
 require "analytic_utils"
 require "javascript_utils"
 
+GROUP_BY_MAPPING = {
+  "month"      => {sql_name: '', hash_name: ''},
+  "quarter"    => {sql_name: '', hash_name: ''},  
+  "year"       => {sql_name: '', hash_name: ''},
+  "repository" => {sql_name: 'r.name', hash_name: 'name'},
+  "state"      => {sql_name: '', hash_name: ''},
+  "company"    => {sql_name: 'c.name', hash_name: 'name'},
+  "user"       => {sql_name: 'u.login', hash_name: 'login'}
+}
+
 class ApiController < ApplicationController
 
   def index
     data_request = params[:data_request]
-    timeframe = params[:timeframe]
+    metric = params[:metric]
+    group_by = params[:group_by]
+    month = params[:month]
+    quarter = params[:quarter]
     year = params[:year]
     repo = params[:repo]
     state = params[:state]
+    company = params[:company]
+    user = params[:user]
 
-    if data_request == 'user_chart'
-      prs_by_user = AnalyticUtils.get_pull_request_stats('u.login', 'num_prs', timeframe, year, repo, state)
-      prs_by_user_top_x = AnalyticUtils.top_x_with_rollup(prs_by_user, 'login', 'num_prs', 5, 'others')
-      prs_by_user_pie_str = JavascriptUtils.get_pull_request_stats(prs_by_user_top_x, 'login', 'num_prs')
+    if data_request == 'prs_chart'
+      #group_by = params[:group_by]
+      #u.login vs c.name
+
+      prs_by_user = AnalyticUtils.get_pull_request_stats(GROUP_BY_MAPPING[group_by][:sql_name], 'num_prs', month, quarter, year, repo, state, company, user)
+      prs_by_user_top_x = AnalyticUtils.top_x_with_rollup(prs_by_user, GROUP_BY_MAPPING[group_by][:hash_name], 'num_prs', 5, 'others')
+      prs_by_user_pie_str = JavascriptUtils.get_pull_request_stats(prs_by_user_top_x, GROUP_BY_MAPPING[group_by][:hash_name], 'num_prs')
       render :json => prs_by_user_pie_str
-    elsif data_request == 'user_table'
-      prs_by_user = AnalyticUtils.get_pull_request_stats('u.login', 'num_prs', timeframe, year, repo, state)
-      @table_handle = "user_prs_table"
+    elsif data_request == 'prs_table'
+      prs_by_user = AnalyticUtils.get_pull_request_stats(GROUP_BY_MAPPING[group_by][:sql_name], 'num_prs', month, quarter, year, repo, state, company, user)
+      @table_handle = "prs_table"
       @table_data = prs_by_user
-      @label_header = "User"
-      @data_header = "Contributions"
-      @label_index_name = "login"
+      @label_header = GROUP_BY_MAPPING[group_by][:hash_name].titleize
+      @data_header = "\# of Pull Requests"
+      @label_index_name = GROUP_BY_MAPPING[group_by][:hash_name]
       @data_index_name = "num_prs"
       render :partial => "dashboard/hash_as_table"
-    elsif data_request == 'company_chart'
-      prs_by_company = AnalyticUtils.get_pull_request_stats('c.name', 'num_prs', timeframe, year, repo, state)
-      prs_by_company_top_x = AnalyticUtils.top_x_with_rollup(prs_by_company, 'name', 'num_prs', 5, 'others')
-      prs_by_company_pie_str = JavascriptUtils.get_pull_request_stats(prs_by_company_top_x, 'name', 'num_prs')
-      render :json => prs_by_company_pie_str
-    elsif data_request == 'company_table'
-      prs_by_company = AnalyticUtils.get_pull_request_stats('c.name', 'num_prs', timeframe, year, repo, state)
-      @table_handle = "company_prs_table"
-      @table_data = prs_by_company
-      @label_header = "Company"
-      @data_header = "Contributions"
-      @label_index_name = "name"
-      @data_index_name = "num_prs"
-      render :partial => "dashboard/hash_as_table"
-
-    elsif data_request == 'days_elapsed_table'
-      avg_days_elapsed = AnalyticUtils.get_pr_days_elapsed
-      @table_handle = "pr_days_elapsed_table"
-      @table_data = avg_days_elapsed
-      @label_header = "Company"
-      @data_header = "Average Days Elapsed"
-      render :partial => "dashboard/hash_as_table"
-   
-    elsif data_request == 'monthly_line_graph'
-      line_graph = AnalyticUtils.get_timestamps(timeframe, year, repo, state)
-      render :json => "{\"response\": #{line_graph}}"
-
+    # elsif data_request == 'days_elapsed_table'
+    #   avg_days_elapsed = AnalyticUtils.get_prs_days_elapsed
+    #   @table_handle = "prs_days_elapsed_table"
+    #   @table_data = avg_days_elapsed
+    #   @label_header = "Company"
+    #   @data_header = "Average Days Elapsed"
+    #   render :partial => "dashboard/hash_as_table"
+    # elsif data_request == 'monthly_line_graph'
+    #   line_graph = AnalyticUtils.get_timestamps(quarter, year, repo, state)
+    #   render :json => "{\"response\": #{line_graph}}"
     else
       render :text => "Error: Invalid data_request: #{data_request}"
     end
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,19 +4,23 @@ require "javascript_utils"
 class DashboardController < ApplicationController
 
   def index
-    @prs_by_user = AnalyticUtils.get_pull_request_stats('u.login', 'num_prs')
-    prs_by_user_top_x = AnalyticUtils.top_x_with_rollup(@prs_by_user, 'login', 'num_prs', 5, 'others')
-    @prs_by_user_pie_str = JavascriptUtils.get_pull_request_stats(prs_by_user_top_x, 'login', 'num_prs')
+    # pull_request data by company as the default
+    @prs_data = AnalyticUtils.get_pull_request_stats('c.name', 'num_prs')
+    prs_top_x = AnalyticUtils.top_x_with_rollup(@prs_data, 'name', 'num_prs', 5, 'Others')
+    @prs_chart_str = JavascriptUtils.get_pull_request_stats(prs_top_x, 'name', 'num_prs')
 
-    @prs_by_company = AnalyticUtils.get_pull_request_stats('c.name', 'num_prs')
-    prs_by_company_top_x = AnalyticUtils.top_x_with_rollup(@prs_by_company, 'name', 'num_prs', 5, 'Others')
-    @prs_by_company_pie_str = JavascriptUtils.get_pull_request_stats(prs_by_company_top_x, 'name', 'num_prs')
+    @prs_table_label_header = 'Company'
+    @prs_table_data_header = 'Contributions'
+    @prs_label_index_name = 'name'
+    @prs_data_index_name = 'num_prs'
 
-    @avg_days_elapsed = AnalyticUtils.get_pr_days_elapsed
+    #@avg_days_elapsed = AnalyticUtils.get_prs_days_elapsed
 
     @repos = Repo.order(:name)
+    @companies = Company.order(:name)
+    @users = User.order(:login)
 
-    @timestamps = AnalyticUtils.get_timestamps
+    #@timestamps = AnalyticUtils.get_timestamps
   end
   
 end
