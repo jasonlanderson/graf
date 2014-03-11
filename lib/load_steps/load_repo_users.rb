8ca3e770a91dfb15e088d0144bd61c6ccfb5b70f
Kalonji Bankole <kkbankol@us.ibm.com>
2014-03-11 10:33:14 -0700

    Fixed load repo users failure

--- lib/load_steps/load_repo_users.rb
+++ lib/load_steps/load_repo_users.rb
@@ -16,26 +16,28 @@ class LoadRepoUsers < LoadStep
     client = OctokitUtils.get_octokit_client
     GithubLoad.log_current_msg("***Loading Users", LogLevel::INFO)
     
-    # Look through the contributors first
-    contributors = client.contributors(repo.full_name)     
-    contributors.each {|user| 
-      unless User.find_by(login: user[:login].downcase) 
-        puts "Creating record for User #{user[:login]}"
-        user_obj = client.user(user[:login]) 
-        LoadHelpers.create_user_if_not_exist(user_obj)
-      end  
-    }
-
-    # Look through the collaborators second
+    # Contributors are those who have submitted at least one commit to the repo
+    contributors = client.contributors(repo.full_name)
+    if contributors     
+      contributors.each {|user| 
+        unless User.find_by(login: user[:login].downcase) 
+          puts "Creating record for User #{user[:login]}"
+          user_obj = client.user(user[:login]) 
+          LoadHelpers.create_user_if_not_exist(user_obj)
+        end  
+      }
+    end
+    # Collaborators are those that have direct commit access
     collaborators = client.collaborators(repo.full_name)     
-    collaborators.each {|user| 
-      unless User.find_by(login: user[:login].downcase) 
-        puts "Creating record for User #{user[:login]}"
-        user_obj = client.user(user[:login]) 
-        LoadHelpers.create_user_if_not_exist(user_obj)
-      end  
-    }
-
+    if collaborators
+      collaborators.each {|user| 
+        unless User.find_by(login: user[:login].downcase) 
+          puts "Creating record for User #{user[:login]}"
+          user_obj = client.user(user[:login]) 
+          LoadHelpers.create_user_if_not_exist(user_obj)
+        end  
+      }
+    end
     puts "Finish Step: #{name}" 
   end
 
