14dd66c7bd3eb28dad981c7ad8d0dbc81e07cd96
Kalonji Bankole <kkbankol@us.ibm.com>
2014-03-17 14:29:30 -0700

    Modified restrictSelections function

--- spec/lib/load_steps/load_helpers_spec.rb
+++ spec/lib/load_steps/load_helpers_spec.rb
@@ -11,10 +11,38 @@ describe LoadHelpers do
   #	expect LoadHelpers.format_name(name).to equal "Kalonji Bankole"
   #end
 
-  company = "IBM Corporation"
- 
+  # self.merge
   it 'merges similar companies' do
-	expect(LoadHelpers.merge(company)).to match("IBM")
+     company = "IBM Corporation"
+	   expect(LoadHelpers.merge(company)).to match("IBM")
   end
 
+  # self.create_user_if_not_exist
+
+    # Test returns user object that already exists
+    # pr_user = User.all[0] #client.user("kkbankol")
+
+
+    # Test creates user object that doesn't exist
+    # name = "some user not in db"
+    # pr_user = client.user(name)
+    # LoadHelpers.create_user_if_not_exist(pr_user)
+    # 
+
+    # Test associating company with a user (should become method)
+
+
+
+  it 'searching commit author'
+     name = "Kalonji Bankole"
+     LoadHelpers.search_name()
+
+     name = "Matthew"
+
+  end
+
+
+  # Test that there are no companies with nil or "" as a name
+
+  # Test that there are no users with nil or "" as a name
 end
