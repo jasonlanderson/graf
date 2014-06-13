3da409bb59049e4a65212d8d909ce53d5004d508
Kalonji Bankole <kkbankol@us.ibm.com>
2014-06-12 17:23:22 -0700

    Fixed reports table css

--- lib/load_steps/delta_load_repo_pull_requests.rb
+++ lib/load_steps/delta_load_repo_pull_requests.rb
@@ -28,11 +28,11 @@ class DeltaLoadRepoPullRequests < LoadStep
       #JSON.parse(HTTParty.get("https://api.github.com/search/issues?q=repo:cloudfoundry/bosh-lite+type:pr+updated:%3E2014-05-27+client_id:949149798908ec942301+client_secret:70563cd761fafd0df22b5f4cb40a68b2b9afc9f4", :headers => {"User-Agent" => "kkbankol"} ).body)["items"]
       #JSON.parse(HTTParty.get("https://api.github.com/search/issues?q=repo:cloudfoundry/bosh-lite+type:pr+updated:%3E2014-05-27", :headers => {"User-Agent" => "kkbankol"} ).body)["items"]
       sleep(3.0)
-      #pull_requests = client.search_issues(repo.full_name, options = {:type => "pr", :updated => "2014-05-27"})[:items]
-      pull_requests = JSON.parse(HTTParty.get("https://api.github.com/search/issues?q=repo:#{repo.full_name}+type:pr+updated:%3E#{parsed_date}", :headers => {"User-Agent" => "kkbankol"} ).body)["items"]
-      puts "Making request to"
-      puts "https://api.github.com/search/issues?q=repo:#{repo.full_name}+type:pr+updated:%3E#{parsed_date}"
-      puts "PULL REQUESTS #{pull_requests}" if repo.full_name.include?("cloudfoundry/bosh-lite")
+      pull_requests = client.search_issues("repo=#{repo.full_name}+type:pr") #.search_issues(repo.full_name, options = {:type => "pr", :updated => "2014-05-27"})[:items]
+      #pull_requests = JSON.parse(HTTParty.get("https://api.github.com/search/issues?q=repo:#{repo.full_name}+type:pr+updated:%3E#{parsed_date}", :headers => {"User-Agent" => "kkbankol"} ).body)["items"]
+      #puts "Making request to"
+      #puts "https://api.github.com/search/issues?q=repo:#{repo.full_name}+type:pr+updated:%3E#{parsed_date}"
+      #puts "PULL REQUESTS #{pull_requests}" if repo.full_name.include?("cloudfoundry/bosh-lite")
     rescue => e
       GithubLoad.log_current_msg("The following error occured...", LogLevel::ERROR)
       GithubLoad.log_current_msg(e, LogLevel::ERROR)
@@ -40,10 +40,10 @@ class DeltaLoadRepoPullRequests < LoadStep
       return nil  
     end
     if !pull_requests.nil? && pull_requests.length > 0
-      pull_requests.each { |pull|
+      pull_requests.each { |pr|
         record, user = nil
         # Make pull_requests hash keys accessible as symbols
-        pr = pull.with_indifferent_access
+        #pr = pull.with_indifferent_access
         # Get user and insert if doesn't already exist
         user = LoadHelpers.create_user_if_not_exist(pr[:user]) if pr[:user]
         #puts "NUMBER #{pr[:number]}, REPO_ID #{Repo.find_by(full_name: repo.full_name).id}, TITLE #{pr[:title]}"
