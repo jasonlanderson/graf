9d823f08e8f6d56d44ebe307df9f7f0647f78bd0
jackfengibm <jackfeng@us.ibm.com>
2014-10-23 16:56:42 -0700

    give reloader a working directory

--- lib/scripts/reload_data.sh
+++ lib/scripts/reload_data.sh
@@ -5,22 +5,25 @@ HOST_INFO="127.0.0.1:3000"
 HOST_NAME_INFO="localhost:3000"
 USER_NAME=foo
 PASSWORD=hello
+WORK_DIR = $HOME
 
 ## don't modify the lines below
 DELETE_PATH=delete_all_data
 LOAD_PATH=start_load
 BASE_URL=http://$HOST_INFO
 
+cd $HOME
+
 ## login
 echo
 echo "- authenticate -"
-curl -o login.html -c cookie.txt -H "Accept:application/json" -X POST ${BASE_URL}/sessions -F "email=${USER_NAME}" -F "password=${PASSWORD}" -F "commit=Log in" -F "utf8=true", -H "Host:${HOST_NAME_INFO}", -H "Origin: ${BASE_URL}", -H "Referer: ${BASE_URL}/login"
+curl -o ${WORK_DIR}/login.html -c ${WORK_DIR}/cookie.txt -H "Accept:application/json" -X POST ${BASE_URL}/sessions -F "email=${USER_NAME}" -F "password=${PASSWORD}" -F "commit=Log in" -F "utf8=true", -H "Host:${HOST_NAME_INFO}", -H "Origin: ${BASE_URL}", -H "Referer: ${BASE_URL}/login"
 
 sleep 5
 ## delete all data
 echo
 echo "- Deleting All Data -"
-curl -o delete_all_data.html -b cookie.txt -H "Accept:application/json" -X GET ${BASE_URL}/${DELETE_PATH} -F "email=${USER_NAME}" -F "password=${PASSWORD}" -F "commit=Log in" -F "utf8=true", -H "Host:${HOST_NAME_INFO}", -H "Origin: ${BASE_URL}", -H "Referer: ${BASE_URL}/" 
+curl -o ${WORK_DIR}/delete_all_data.html -b ${WORK_DIR}/cookie.txt -H "Accept:application/json" -X GET ${BASE_URL}/${DELETE_PATH} -F "email=${USER_NAME}" -F "password=${PASSWORD}" -F "commit=Log in" -F "utf8=true", -H "Host:${HOST_NAME_INFO}", -H "Origin: ${BASE_URL}", -H "Referer: ${BASE_URL}/" 
 
 sleep 5
 
@@ -28,4 +31,4 @@ sleep 5
 echo
 echo "- Loading All Data -"
 echo
-curl -o load.html -b cookie.txt -H "Accept:application/json" -X GET ${BASE_URL}/${LOAD_PATH} -F "email=${USER_NAME}" -F "password=${PASSWORD}" -F "commit=Log in" -F "utf8=true", -H "Host:${HOST_NAME_INFO}", -H "Origin: ${BASE_URL}", -H "Referer: ${BASE_URL}/load"
+curl -o ${WORK_DIR}/load.html -b ${WORK_DIR}/cookie.txt -H "Accept:application/json" -X GET ${BASE_URL}/${LOAD_PATH} -F "email=${USER_NAME}" -F "password=${PASSWORD}" -F "commit=Log in" -F "utf8=true", -H "Host:${HOST_NAME_INFO}", -H "Origin: ${BASE_URL}", -H "Referer: ${BASE_URL}/load"
