c295c5664351bda74ad720cabe04d708a6aace21
jackfengibm <jackfeng@us.ibm.com>
2014-10-29 15:33:23 -0700

    misc changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,6 +34,13 @@ Graf::Application.configure do
   # Version of your assets, change this if you want to expire all your assets.
   # config.assets.version = '1.0'
 
+  # Specifies the header that your server uses for sending files.
+  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
+  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+
+  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+  # config.force_ssl = true
+ 
   # Set to :debug to see everything in the log.
   # config.log_level = :debug
   config.log_level = :info
